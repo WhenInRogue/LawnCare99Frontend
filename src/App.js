d9d1196d178300ccfb952f3266fe46be43704248
WhenInRogue <Zajomapijoseph@gmail.com>
2025-11-08 12:54:31 -0600

    third commit: Login and Register

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ProtectedRoute, AdminRoute } from "./service/Guard";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Routes>
+        <Route path="/register" element={<RegisterPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+
+        {/* ADMIN ROUTES */}
+
+
+          {/* ADMIN AND MANAGERS ROUTES */}
+        
+
+
+
+
+
+        
+
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
