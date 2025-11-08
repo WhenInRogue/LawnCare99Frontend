1f6adac87e2e73c9afd5b766b330278f07ebe523
WhenInRogue <Zajomapijoseph@gmail.com>
2025-11-08 14:05:06 -0600

    fourth commit: Base Layout

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ProtectedRoute, AdminRoute } from "./service/Guard";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
 
 
 function App() {
@@ -16,10 +17,14 @@ function App() {
 
 
           {/* ADMIN AND MANAGERS ROUTES */}
+
+
+          <Route path="/profile" element={<ProtectedRoute element={<ProfilePage/>}/>}/>
+
+
+          //catch-all route, redirects users to the login page when they enter an invalid path
+          <Route path="*" element={<LoginPage/>}/>
         
-
-
-
 
 
         
